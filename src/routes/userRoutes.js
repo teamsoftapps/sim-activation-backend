91ba426b13089ead4bbcea501dcf1166cf91e037
SoftApps <66966682+teamsoftapps@users.noreply.github.com>
2025-07-10 05:35:17 +0500

    5:35

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,16 +1,18 @@
-import express from "express";
-import User from "../models/User.js";
+/** @format */
+
+import express from 'express';
+import User from '../models/User.js';
 
 const router = express.Router();
 
 // GET /user/current?email=abc@example.com
-router.get("/current", async (req, res) => {
+router.get('/current', async (req, res) => {
   const { email } = req.query;
 
   if (!email) {
     return res
       .status(400)
-      .json({ success: false, message: "Email is required" });
+      .json({ success: false, message: 'Email is required' });
   }
 
   try {
@@ -19,7 +21,7 @@ router.get("/current", async (req, res) => {
     if (!user) {
       return res
         .status(404)
-        .json({ success: false, message: "User not found" });
+        .json({ success: false, message: 'User not found' });
     }
 
     return res.status(200).json({
@@ -32,32 +34,35 @@ router.get("/current", async (req, res) => {
       },
     });
   } catch (err) {
-    console.error("❌ Error fetching current user:", err);
-    res.status(500).json({ success: false, message: "Server error" });
+    console.error('❌ Error fetching current user:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
   }
 });
 
 // GET /user/activations?email=abc@example.com
-router.get("/activations", async (req, res) => {
+router.get('/activations', async (req, res) => {
   const { email } = req.query;
 
   if (!email) {
     return res
       .status(400)
-      .json({ success: false, message: "Email is required" });
+      .json({ success: false, message: 'Email is required' });
   }
 
   try {
     const user = await User.findOne({ email }).lean();
 
+    console.log('user data:', user);
+
     if (!user) {
       return res
         .status(404)
-        .json({ success: false, message: "User not found" });
+        .json({ success: false, message: 'User not found' });
     }
 
     const activations = user.activationData.map((activation) => ({
       esn: activation.esn,
+      mdn: activation.mdn || null,
       STREET1: activation.E911ADDRESS?.STREET1 || null,
       STREET2: activation.E911ADDRESS?.STREET2 || null,
       CITY: activation.E911ADDRESS?.CITY || null,
@@ -71,8 +76,8 @@ router.get("/activations", async (req, res) => {
       activations,
     });
   } catch (err) {
-    console.error("❌ Error fetching activations:", err);
-    res.status(500).json({ success: false, message: "Server error" });
+    console.error('❌ Error fetching activations:', err);
+    res.status(500).json({ success: false, message: 'Server error' });
   }
 });
 export default router;
