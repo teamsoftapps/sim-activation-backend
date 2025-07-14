60dedf2e6c5241a7669f42dcf515535f00caacd4
Development <66966682+teamsoftapps@users.noreply.github.com>
2025-07-15 03:15:12 +0500

    Update app.js

--- app.js
+++ app.js
@@ -61,12 +61,7 @@ app.use(
   cors({
     origin: "https://www.jf-mobile.com", // allow only your frontend
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: [
-      "Content-Type",
-      "Authorization",
-      "x-api-key",
-      "Access-Control-Allow-Origin",
-    ],
+    allowedHeaders: ["Content-Type", "Authorization", "x-api-key"],
     credentials: true,
   })
 );
